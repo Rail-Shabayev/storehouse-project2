fc4c400920ed87d96808537d90aeb52c9cb84899
rail <rail.shabayev@mail.ru>
2024-09-26 07:48:46 +0400

    chore: add initial files
    Took 5 minutes

---
+++ src/test/java/com/example/rail/RailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
