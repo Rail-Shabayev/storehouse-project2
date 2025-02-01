77982756d2aa1cf4ebecbafc3839a851c176d5fc
Rail <rail.shabayev@mail.ru>
2025-02-01 12:55:40 +0400

    feat: add validation
    4 point is done
    
    Took 9 minutes

--- src/main/java/com/example/rail/dto/product/ProductDto.java
+++ src/main/java/com/example/rail/dto/product/ProductDto.java
@@ -2,6 +2,7 @@ package com.example.rail.dto.product;
 
 
 import com.example.rail.model.Category;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,12 +17,29 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductDto {
+    @NotNull
+    @NotBlank
     private String name;
+    @NotNull
+    @NotBlank
     private Long article;
+    @NotNull
     private String description;
+    @NotNull
+    @NotBlank
     private Category category;
+    @NotNull
+    @NotBlank
+    @DecimalMin(value = "0")
     private BigDecimal price;
+    @NotNull
+    @NotBlank
+    @PositiveOrZero
     private int quantity;
+    @NotNull
+    @NotBlank
     private LocalDateTime updatedAt;
+    @NotNull
+    @NotBlank
     private LocalDate createdAt;
 }
